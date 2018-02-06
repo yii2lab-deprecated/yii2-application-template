eccc38cd909d48cf7438669870927865b6789606
Vitaliy <theyamshikov@yandex.ru>
2018-02-06 13:00:44 +0600

    move files

---
+++ src/common/assets/main/MainAsset.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace yii2lab\applicationTemplate\common\assets\main;
+
+use yii\web\AssetBundle;
+
+class MainAsset extends AssetBundle
+{
+	public $depends = [
+		'yii\web\YiiAsset',
+		'yii2lab\applicationTemplate\common\assets\fontAwesome\BowerAsset',
+		'yii2lab\ubuntu_font\assets\UbuntuAsset',
+		'yii2lab\applicationTemplate\common\assets\main\ScriptAsset',
+		'yii2lab\applicationTemplate\common\assets\main\StyleAsset',
+	];
+}
