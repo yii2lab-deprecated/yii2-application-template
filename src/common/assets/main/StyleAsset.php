8e070244c2c2149183bddab8e6870d842598fda8
Vitaliy <theyamshikov@yandex.ru>
2018-02-22 12:00:37 +0600

    refacto styles

--- src/common/assets/main/StyleAsset.php
+++ src/common/assets/main/StyleAsset.php
@@ -2,12 +2,18 @@
 
 namespace yii2lab\applicationTemplate\common\assets\main;
 
+use Yii;
 use yii\web\AssetBundle;
 
 class StyleAsset extends AssetBundle
 {
 	public $sourcePath = '@yii2lab/applicationTemplate/common/assets/main/dist';
 	public $css = [
+		'css/text.css',
 		'css/main.css',
+		'css/grid-view.css',
+		'css/align-logout-button.css',
+		'css/jumbotron.css',
 	];
+
 }
