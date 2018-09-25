1d575f8474790725857940e3bbf87e7a1aa9c1e5
Vitaliy <theyamshikov@yandex.ru>
2018-09-25 12:54:26 +0600

    make mock asset

---
+++ src/common/assets/mock/BlankAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace yii2lab\applicationTemplate\common\assets\mock;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class BlankAsset
+ *
+ * Define in env config
+ *
+ *```php
+ * 'container' => [
+ *      'yii\bootstrap\BootstrapAsset' => [
+ *          'class' => 'yii2lab\applicationTemplate\common\assets\mock\BlankAsset',
+ *      ],
+ * ],
+ * ```
+ *
+ * @package yii2lab\applicationTemplate\common\assets\mock
+ *
+ */
+class BlankAsset extends AssetBundle {
+
+}
