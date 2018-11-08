4e1a6d21dde7ab937fef66ed87ea5512d10df801
Vitaliy <theyamshikov@yandex.ru>
2018-11-08 12:45:11 +0600

    make lang

---
+++ src/common/messages/ru/admin.php
@@ -0,0 +1,26 @@
+<?php
+return [
+	
+	'gii' => 'Gii',
+	
+	'logreader' => 'Логи',
+	
+	'modules' => 'Модули',
+	
+	'main' => 'Основное',
+	'system' => 'Система',
+	'develop' => 'Разработка',
+    'tools' => 'Инструменты',
+	
+	'app' => 'Приложение',
+	//'app_cache' => 'Кэш',
+	//'app_lang' => 'Язык',
+	
+	'rbac' => 'RBAC',
+	'rbac_permission' => 'Разрешения',
+	'rbac_role' => 'Роли',
+	'rbac_rule' => 'Правила',
+	'rbac_assignment' => 'Назначения',
+	
+
+];
