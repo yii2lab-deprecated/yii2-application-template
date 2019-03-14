db2fffb20f77e7e435e397f0d55db6fd09a5cfa9
amubarak <amubarak@wooppay.com>
2019-03-14 16:34:58 +0600

    messages uz

--- src/common/messages/uz/main.php
+++ src/common/messages/uz/main.php
@@ -1,6 +1,188 @@
-<?php
-
+﻿<?php
 return [
-	'error' => 'Хато',
-	'under_consideration' => 'Кўриб чиқилмоқда',
+	
+	'main_page' => 'Асосий саҳифа',
+	
+	'documentation' => 'Документация',
+	
+	'not_found_collection' => 'Бирор бир ёзув топилмади',
+	
+	'error' => 'Хатолик',
+	'info' => 'Маълумот',
+	
+	'id' => 'ID',
+	'type' => 'Тури',
+	'name' => 'Номи',
+	'content' => 'Таркиби',
+	
+	'go_to_backend' => 'Админка ичига',
+	'go_to_frontend' => 'Сайт ичига',
+	
+	'author' => 'Автор',
+	'all' => 'Барчаси',
+	'title' => 'Номи',
+	'description' => 'Тасвирлаш',
+	
+	'sort' => 'Тартиблаш',
+	'hidden' => 'Яширинган',
+	'readonly' => 'Фақат ўқиш учун',
+	'mask' => 'Ниқоб',
+	'value' => 'Мазмун',
+	'button' => 'Тугма',
+	'picture' => 'Картинка',
+	'blacklist' => 'Қора рўйхат',
+	
+	'list_title' => 'Рўйхат',
+	'create_title' => 'Яратмоқ',
+	'update_title' => 'Тўғирлаш',
+	'delete_title' => 'Ўчириш',
+	
+	'create_success' => 'Муваффақиятли яратилди',
+	'update_success' => Муваффақиятли яратилди',
+	'delete_success' => Муваффақиятли ўчирилди',
+	
+	'save' => 'Сақлаш',
+	'create' => 'Яратмоқ',
+	'update' => 'Тўғирлаш',
+	'delete' => 'Ўчириш',
+	'clear' => 'Тозалаш',
+	
+	'urgently' => 'Зудлик билан',
+	'complete' => 'Якунланди',
+	
+	'priority' => 'Приоритет',
+	'priority_low' => 'Паст',
+	'priority_normal' => 'Ўрта',
+	'priority_hight' => 'Юқори',
+	
+	'category' => 'Категория',
+	
+	'readonly_warning' => 'Контент ёзув киритишдан ҳимояланган',
+	
+	
+	'created_at' => 'Яратиш вақти',
+	'updated_at' => 'Тўғирлаш вақти',
+	'deleted_at' => 'Ўчириш вақти',
+	
+	'empty' => 'Бўш',
+	
+	'version' => 'Версия',
+	'message' => 'Хабар',
+	
+	'complete_confirm' => 'Ўчириш учун ишончингиз комилми?',
+	
+	'title_home' => 'Асосий',
+	
+	'created_at {date}' => 'Яратилган {date}',
+	
+	'actions' => 'Ҳаракатлар',
+	'action' => 'Ҳаракат',
+	
+	'yes' => 'Ҳа',
+	'no' => 'Йўқ',
+	
+	//'email'=>'Email',
+	'subject' => 'Хат мавзуси',
+	'body' => 'Хатнинг тексти',
+	
+	'status' => 'Статус',
+	'status_on' => 'Ёқилган',
+	'status_off' => 'Ўчирилган',
+	'created_by' => 'Добавил',
+	'updated_by' => 'Янгиланган',
+	'viewed_at' => 'Кўриб чиқилган',
+	'shipped_at' => 'Жўнатилган',
+	
+	//'password'=>'Пароль',
+	'user' => 'Фойдаланувчи',
+	'date_from' => 'Дата санадан',
+	'date_to' => 'Дата санагача',
+	'error_text' => 'Текстнинг хатоси',
+	'folder' => 'Папка',
+	
+	'url' => 'Ишора',
+	'seo_title' => 'SEO Сарлавҳа',
+	'seo_keywords' => 'SEO Калит сўзлар',
+	'seo_description' => 'SEO Тасвирлаш',
+	
+	'time' => 'Вақт',
+	'ip' => 'IP адрес',
+	'agent' => 'Браузер',
+	
+	'notification' => 'Хабар',
+	'icon' => 'Иконка',
+	'alert_status_no_read' => 'Ўқилмаган',
+	'alert_status_read' => 'Ўқилган',
+	'alert_type_default' => 'Олдиндан хабар бериш',
+	'alert_type_info' => 'Хабар бериш',
+	'alert_type_warning' => 'Огоҳлантириш',
+	'alert_type_danger' => 'Хабар',
+	
+	'class' => 'Класс',
+	'count' => 'Сони',
+	
+	'comment' => 'Изоҳ',
+	
+	'width' => Эни',
+	'height' => 'Баландлиги',
+	
+	'does_not_exist' => 'Мавжуд эмас',
+	'under_consideration' => 'Кўриб чиқиш жараёнида',
+	'approved' => 'Тасдиқланган',
+	'undefined' => 'Аниқланмаган',
+	'control' => 'Бошқарув',
+	'city' => 'Шаҳар',
+	'partner' => 'Ҳамкор',
+	'phone_number' => 'Телефон',
+	'fio' => 'ФИШ',
+	'email' => 'Электрон почта',
+	'signin' => 'Кириш',
+	'password' => 'Пароль',
+	'remember_me' => 'Эслаб қолиш',
+	'login' => 'Логин',
+	'registered_date' => 'Рўйхатдан ўтиш санаси',
+	'address' => 'Манзили',
+	'activity' => 'Фаолият тури',
+	'cabinet' => 'Кабинет',
+	'sign_up_verb' => 'Рўйхатдан ўтиш',
+	'cancel' => 'Бекор қилиш',
+	'create_time' => 'Яратиш санаси',
+	'contact_data' => 'Боғланиш учун маълумотлар',
+	'commission' => 'Комиссия',
+	'sign_out' => 'Чиқиш',
+	'sign_in' => 'Кириш',
+	'not_found' => 'Объект топилмади',
+
+	'payments_without_reg' => 'Рўйхатдан ўтмасдан тўловларни амалга ошириш',
+	'popular_services' => 'Оммабоп хизматлар',
+	'mobile_connection' => 'Мобил алоқа',
+	'search_placeholder' => 'Хизмат турини қидириш',
+	'lang_switch_mobile' => 'Версия <br> телефон учун',
+	'about_wooppay' => 'Wooppay ҳақида',
+	'for_business' => 'Бизнес учун',
+	'all_rights reserved' => 'WOOPPAY © 2012 - 2018. Барча ҳуқуқлар ҳимояланган',
+	'client_agreement' => 'Мижоз келишуви',
+	'total_to_pay' => 'Тўлов учун',
+	'complete_button' => 'Давом эттириш',
+	'important_information' => 'Телефон рақамингиз ва Email манзилингизни кўрсатинг. Email манзилингизга чек юборилади, телефон рақамингиз тўлов билан боғлиқ муаммо юзага келганда зарур бўлади.',
+	'your_phone_number' => 'Мобил телефонингиз рақами',
+	'subscriber_number' => 'Абонент рақами',
+	'amount' => 'Сумма',
+	'to_pay' => 'Тўлов учун (барчаси)',
+	'check' => 'ЧЕК',
+	'back_to_pay' => 'ТЎЛОВГА ЎТИШ',
+	'sorry_for_waiting' => 'Кутиб қолганингиз учун узр, кейинроқ такрорланг ёки қўллаб қувватлаш хизматига мурожаат қилинг <a href="#">қўллаб қувватлаш хизмати</a>',
+	'button_repeat' => 'Такрорлаш',
+	'data_about_payment' => 'Тўлов тўғрисида маълумотлар',
+	'card_mask' => 'Карта маскаси:',
+	'cardholder_name' => 'Карта сақловчисининг исми:',
+	'total_amount' => 'Тўлов суммаси:',
+	'code' => 'Код',
+	'complete_payment' => 'Тўловни давом эттириш',
+	'send_code_again' => 'Кодни такроран юбориш',
+
+
+
+
+
 ];
